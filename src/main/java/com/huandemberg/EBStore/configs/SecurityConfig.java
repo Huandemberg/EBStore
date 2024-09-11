3b71ce84f31ab330b44b1295498dff0c5bdacc52
Huandemberg <128419405+Huandemberg@users.noreply.github.com>
2024-09-11 15:39:26 -0300

    Finally Controllers and security

---
+++ src/main/java/com/huandemberg/EBStore/configs/SecurityConfig.java
@@ -0,0 +1,92 @@
+package com.huandemberg.EBStore.configs;
+
+import com.huandemberg.EBStore.security.JWTAuthenticationFilter;
+import com.huandemberg.EBStore.security.JWTAuthorizationFilter;
+import com.huandemberg.EBStore.security.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    private static final String[] PUBLIIC_MATCHERS = {
+
+            "/"
+
+    };
+
+    private static final String[] PUBLIIC_MATCHERS_POST = {
+            "/user",
+            "/login"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable);
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(this.userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder());
+        this.authenticationManager = authenticationManagerBuilder.build();
+
+        http.authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers(HttpMethod.POST, PUBLIIC_MATCHERS_POST).permitAll()
+                .requestMatchers(PUBLIIC_MATCHERS).permitAll()
+                .anyRequest().authenticated()).authenticationManager(authenticationManager);
+
+
+        http.addFilter(new JWTAuthenticationFilter(this.authenticationManager, this.jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(this.authenticationManager, this.jwtUtil, this.userDetailsService));
+
+        http.sessionManagement((sessions) -> sessions
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
---
+++ src/main/java/com/huandemberg/EBStore/controllers/ProdutoController.java
@@ -0,0 +1,59 @@
+package com.huandemberg.EBStore.controllers;
+
+import com.huandemberg.EBStore.models.Produto;
+import com.huandemberg.EBStore.models.projection.ProdutoProjection;
+import com.huandemberg.EBStore.services.ProdutoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/produto")
+@Validated
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id){
+        Produto obj = this.produtoService.findById(id);
+        return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping("/produtos")
+    public ResponseEntity<List<ProdutoProjection>> findAllByVendas(){
+        List<ProdutoProjection> objs = this.produtoService.findAllByProdutos();
+        return ResponseEntity.ok().body(objs);
+    }
+
+    @PostMapping
+    @Validated
+    public ResponseEntity<Void> create(@Valid @RequestBody Produto obj){
+        this.produtoService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    @Validated
+    public ResponseEntity<Void> update(@Valid @RequestBody Produto obj, @PathVariable Long id) {
+        obj.setId(id);
+        this.produtoService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.produtoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
