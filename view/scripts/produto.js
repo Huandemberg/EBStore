f9c565d0efb05617f24b9c83688cf24c26ba214e
Huandemberg <128419405+Huandemberg@users.noreply.github.com>
2024-10-14 17:25:08 -0300

    Update produtos implement

--- view/scripts/produto.js
+++ view/scripts/produto.js
@@ -21,10 +21,80 @@ function show(produtos) {
                     <td>${produto.tamanho}</td>
                     <td>${produto.cor}</td>
                     <td>${produto.preco}</td>
+                    <td><button type="button" onclick="getProduto(${produto.id})" class="btn btn-primary">Alterar</button></td>
                 </tr> `;
     }
 
     document.getElementById("produtos").innerHTML = tab;
+}
+
+function alterar(produto) {
+    tab = `<form>
+                <div class="mb-3">
+                    <label class="form-label">Modelo</label>
+                    <input type="text" value="${produto.modelo}" class="form-control" id="modelInput" aria-describedby="form-control" autocomplete="off">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Tamanho</label>
+                    <input type="text" value=${produto.tamanho} class="form-control" id="tamanhoInput">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Cor</label>
+                    <input type="text" value="${produto.cor}" class="form-control" id="corInput">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Preço</label>
+                    <input type="number" value=${produto.preco} class="form-control" id="valorInput">
+                </div>
+                <button type="button" onclick="setProduto(${produto.id})" class="btn btn-primary">Submit</button>
+            </form>`;
+
+            document.getElementById("produtos").innerHTML = tab;
+}
+
+async function setProduto(id){
+
+
+    const produtoEndpoint1 = "http://localhost:8080/produto/" + id;
+    let modelo = document.getElementById("modelInput").value;
+    let cor = document.getElementById("corInput").value;
+    let tamanho = document.getElementById("tamanhoInput").value;
+    let preco = document.getElementById("valorInput").value; 
+    let key = "Authorization";
+    const response = await fetch(produtoEndpoint1, {
+        method: "PUT",
+        headers: new Headers({
+            "Content-Type": "application/json; charset=utf8",
+            Accept: "application/json",
+            Authorization: localStorage.getItem(key),
+
+        }),
+        body: JSON.stringify({
+            modelo: modelo,
+            tamanho: tamanho,
+            cor: cor,
+            preco: preco
+        }),
+    });
+    window.location = "/view/produto.html";
+
+}
+
+async function getProduto(id) {
+    const produtoEndpoint1 = "http://localhost:8080/produto/" + id;
+    let key = "Authorization";
+    const response = await fetch(produtoEndpoint1, {
+        method: "GET",
+        headers: new Headers({
+            Authorization: localStorage.getItem(key),
+
+        }),
+    });
+
+    var data = await response.json();
+    console.log(data);
+    if (response) hideLoader();
+    alterar(data);
 }
 
 async function getProdutos() {
--- view/scripts/script.js
+++ view/scripts/script.js
@@ -2,3 +2,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
     if (!localStorage.getItem("Authorization"))
       window.location = "/view/login.html";
   });
+
+  function logout(){
+    localStorage.clear();
+    window.location = "/view/login.html";
+  }
